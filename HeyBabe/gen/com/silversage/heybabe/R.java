de8d9221be439f0f2fb942871a68e51b508b0e77
zainmemon <zainumemon@gmail.com>
2014-03-01 13:33:21 +0500

    test commit 2

--- HeyBabe/gen/com/silversage/heybabe/R.java
+++ HeyBabe/gen/com/silversage/heybabe/R.java
@@ -24,18 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int detail=0x7f080000;
     }
     public static final class layout {
         public static final int activity_dashboard=0x7f030000;
+        public static final int registration=0x7f030001;
+        public static final int registration_1=0x7f030002;
     }
     public static final class menu {
         public static final int dashboard=0x7f070000;
+        public static final int registarion=0x7f070001;
+        public static final int registration=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_registarion=0x7f050003;
+        public static final int title_activity_registration=0x7f050004;
     }
     public static final class style {
         /** 

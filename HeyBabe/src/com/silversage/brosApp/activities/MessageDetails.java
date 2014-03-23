d68d47ef6459406ce43d06e8ad87291da58bf2fb
zainmemon <zainumemon@gmail.com>
2014-03-23 11:31:34 +0500

    message details xml done

---
+++ HeyBabe/src/com/silversage/brosApp/activities/MessageDetails.java
@@ -0,0 +1,15 @@
+package com.silversage.brosApp.activities;
+
+import com.silversage.brosApp.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MessageDetails extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.message_details);
+
+	}
+
+}

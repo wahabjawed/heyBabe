e81b190ea05a574069f6034224a15a7fef4ee8d9
zainmemon <zainumemon@gmail.com>
2014-03-30 15:56:25 +0500

    conditions screen done

---
+++ HeyBabe/src/com/silversage/brosApp/activities/Conditions.java
@@ -0,0 +1,15 @@
+package com.silversage.brosApp.activities;
+
+import com.silversage.brosApp.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Conditions extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.conditions);
+
+	}
+
+}

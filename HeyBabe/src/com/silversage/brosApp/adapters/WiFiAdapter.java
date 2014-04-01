1c1ee615e5d1a13826d34ecf33c53d58eba82a23
Wahab Jawed <wahabjawed@gmail.com>
2014-04-02 01:29:48 +0500

    Cross Button solved

--- HeyBabe/src/com/silversage/brosApp/adapters/WiFiAdapter.java
+++ HeyBabe/src/com/silversage/brosApp/adapters/WiFiAdapter.java
@@ -7,13 +7,16 @@ import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.silversage.brosApp.R;
+import com.silversage.brosApp.activities.Conditions;
 import com.silversage.brosApp.objects.adapters.WiFiObject;
 
 public class WiFiAdapter extends ArrayAdapter<WiFiObject> {
@@ -31,11 +34,12 @@ public class WiFiAdapter extends ArrayAdapter<WiFiObject> {
 
 		public TextView text;
 		public ToggleButton select;
+		public Button delete;
 
 	}
 
 	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(final int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
 		View vi = convertView;
 		final int pos = position;
@@ -50,14 +54,22 @@ public class WiFiAdapter extends ArrayAdapter<WiFiObject> {
 
 			viewHolder.text = (TextView) vi.findViewById(R.id.name);
 			viewHolder.select = (ToggleButton) vi.findViewById(R.id.tog_button);
-
+			viewHolder.delete = (Button) vi.findViewById(R.id.delete);
 			vi.setTag(viewHolder);
 		}
 		ViewHolder holder = (ViewHolder) vi.getTag();
 		WiFiObject obj = data.get(position);
 		holder.text.setText(obj.getName());
 		holder.select.setChecked(obj.isRunWhenConnect());
+		holder.delete.setOnClickListener(new OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				data.remove(position);
+				((Conditions)activity).PreExecute();
+			}
+		});
 		return vi;
 	}
 
